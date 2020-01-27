16dfccbec72f195f30af6bfb02d216ba56b680c3
Nikita Ulko <ulkenauer@gmail.com>
2020-01-27 13:15:46 +0700

    bundle-analyzer + production compression

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,10 +1,31 @@
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
 
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const CompressionPlugin = require('compression-webpack-plugin');
+
 module.exports = merge(common, {
     mode: 'production',
     output: {
       filename: '[name].[chunkhash].js'
     },
+    plugins: [
+      new BundleAnalyzerPlugin(),
+      new CompressionPlugin({
+        filename: '[path].gz[query]',
+        algorithm: 'gzip',
+        test: /\.js$|\.css$|\.html$|\.svg$/,
+        threshold: 10240,
+        minRatio: 0.8,
+      }),
+      new CompressionPlugin({
+        filename: '[path].br[query]',
+        algorithm: 'brotliCompress',
+        test: /\.(js|css|html|svg)$/,
+        compressionOptions: { level: 11 },
+        threshold: 10240,
+        minRatio: 0.8,
+      }),
+    ],
     devtool: 'source-map',
 });
